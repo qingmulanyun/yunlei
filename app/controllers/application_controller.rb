3ecfd800af93f9948697642cad65b815f58c21b2
qingmulanyun <wenbo199019@gmail.com>
2018-10-30 16:51:30 +1100

    landing page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
+	include Pundit
+	protect_from_forgery
+	# Prevent CSRF attacks by raising an exception.
+	# For APIs, you may want to use :null_session instead.
+	before_action :authenticate_user!
+	before_action :configure_permitted_parameters, if: :devise_controller?
+	after_action :verify_authorized, unless: :devise_controller?
+	
 	layout :layout_by_resource
 	
 	private
@@ -9,5 +17,12 @@ class ApplicationController < ActionController::Base
 		else
 			"application"
 		end
+	end
+	
+	protected
+	
+	def configure_permitted_parameters
+		devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+		devise_parameter_sanitizer.permit(:sign_in, keys: [:callback])
 	end
 end
